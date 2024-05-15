2fbcb05c3014c3cb1da2f26d187b9497a214ecdc
kinxyzz <kinxyzz@gmail.com>
2024-05-16 03:02:08 +0700

    feat:adminrolearticle

--- src/components/uploadartikel/UploadArtikel.jsx
+++ src/components/uploadartikel/UploadArtikel.jsx
@@ -1,53 +1,103 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+import { removeFileExtensionFromUrl } from "../../utils/parseImgName";
+
+const initialState = {
+  image: null,
+  judul: "",
+  isi: "",
+  loading: false,
+  penulis: "",
+  headline: "",
+  errorMessage: "",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_IMAGE":
+      return { ...state, image: action.payload };
+    case "SET_JUDUL":
+      return { ...state, judul: action.payload };
+    case "SET_ISI":
+      return { ...state, isi: action.payload };
+    case "SET_LOADING":
+      return { ...state, loading: action.payload };
+    case "SET_PENULIS":
+      return { ...state, penulis: action.payload };
+    case "SET_HEADLINE":
+      return { ...state, headline: action.payload };
+    case "SET_ERROR_MESSAGE":
+      return { ...state, errorMessage: action.payload };
+    default:
+      return state;
+  }
+};
 
 export default function UploadArtikel() {
-  const [image, setImage] = useState(null);
-  const [judul, setJudul] = useState("");
-  const [isi, setIsi] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [penulis, setPenulis] = useState("");
-  const [headline, setHeadline] = useState("");
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { image, judul, isi, loading, penulis, headline, errorMessage } = state;
   const token = localStorage.getItem("token");
+
+  const controller = new AbortController();
+  const signal = controller.signal;
+
   async function handleUpload(e) {
-    setLoading(true);
+    dispatch({ type: "SET_LOADING", payload: true });
     e.preventDefault();
     const formData = new FormData();
     formData.append("file", image);
-    const res = await axios.post("http://localhost:3000/upload", formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
-
+    const res = await axios.post(
+      "http://localhost:3000/image/upload",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        signal,
+      }
+    );
+    const imageUrl = res.data.imageUrl;
+    const imageName = removeFileExtensionFromUrl(imageUrl);
     if (!res.data.imageUrl) {
-      setLoading(false);
+      dispatch({ type: "SET_LOADING", payload: false });
       return;
     }
-    //new
-    const data = {
-      judul,
-      isi,
-      penulis,
-      headline,
-      gambar: res.data.imageUrl,
-    };
+    try {
+      const data = {
+        judul,
+        isi,
+        penulis,
+        headline_isi: headline,
+        gambar: imageUrl,
+      };
 
-    const response = await axios.post("http://localhost:3000/artikel", data, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    });
+      const response = await axios.post("http://localhost:3000/artikel", data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        signal,
+      });
 
-    setLoading(false);
-    alert("Artikel Created");
-    return response;
+      alert("Artikel Created");
+      return response;
+    } catch (error) {
+      if (error.response && error.response.status === 403) {
+        dispatch({
+          type: "SET_ERROR_MESSAGE",
+          payload: error.response.data.message,
+        });
+      }
+      await axios.delete(`http://localhost:3000/image/delete/${imageName}`);
+    } finally {
+      dispatch({ type: "SET_LOADING", payload: false });
+    }
   }
 
   return (
     <div className="flex flex-col gap-4 items-center justify-center h-screen">
       <h2 className="text-3xl text-white">Upload Article</h2>
+      <p className="text-red-600 font-semibold text-sm">{errorMessage}</p>
       <div>
         <form onSubmit={handleUpload} action="">
           <label className="block" htmlFor="fileImage">
@@ -58,7 +108,9 @@ export default function UploadArtikel() {
             name="fileImage"
             type="file"
             disabled={loading}
-            onChange={(e) => setImage(e.target.files[0])}
+            onChange={(e) =>
+              dispatch({ type: "SET_IMAGE", payload: e.target.files[0] })
+            }
             className="file-input file-input-bordered file-input-primary w-full max-w-xs"
           />
 
@@ -73,7 +125,9 @@ export default function UploadArtikel() {
             disabled={loading}
             placeholder="Type here"
             className="input input-bordered input-primary w-full max-w-xs"
-            onChange={(e) => setHeadline(e.target.value)}
+            onChange={(e) =>
+              dispatch({ type: "SET_HEADLINE", payload: e.target.value })
+            }
           />
           <label className="block mt-4" htmlFor="judul">
             judul
@@ -86,7 +140,9 @@ export default function UploadArtikel() {
             disabled={loading}
             placeholder="Type here"
             className="input input-bordered input-primary w-full max-w-xs"
-            onChange={(e) => setJudul(e.target.value)}
+            onChange={(e) =>
+              dispatch({ type: "SET_JUDUL", payload: e.target.value })
+            }
           />
           <label className="block mt-4" htmlFor="isi">
             Content
@@ -95,7 +151,9 @@ export default function UploadArtikel() {
             id="isi"
             value={isi}
             name="isi"
-            onChange={(e) => setIsi(e.target.value)}
+            onChange={(e) =>
+              dispatch({ type: "SET_ISI", payload: e.target.value })
+            }
             className="textarea textarea-primary w-full"
             placeholder="Content"
             disabled={loading}
@@ -111,7 +169,9 @@ export default function UploadArtikel() {
             disabled={loading}
             placeholder="Type here"
             className="input input-bordered input-primary w-full max-w-xs"
-            onChange={(e) => setPenulis(e.target.value)}
+            onChange={(e) =>
+              dispatch({ type: "SET_PENULIS", payload: e.target.value })
+            }
           />
           <button disabled={loading} className="btn mt-4 btn-primary">
             submit
