6a36fe6d53b378e9dac3e5f13bba89b8c9ec0aa3
Akbar-aryansyah <akbar.aryansyah88@gmail.com>
2024-05-16 00:05:55 +0700

    15-05-2024

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,120 +1,73 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import NavbarAdmin from "../navbar/navbaradmin";
-import { getLapors } from "../../redux/actions/datalaporan.action";
-import axios from "axios";
+import React from "react"; 
+import { Link } from "react-router-dom";
+import logoFooter from "../../assets/LogoTanganFooter.png"
+import logoFb from "../../assets/fb.png"
+import logoX from "../../assets/twt.png"
+import logoIg from "../../assets/ig.png"
+import logoSekolah from "../../assets/sman8.png"
+import logoemail from "../../assets/logoemail.png"
+import logophone from "../../assets/phone.png"
 
-function DataLaporan() {
-  const dispatch = useDispatch();
-  const { isLoading, lapors } = useSelector((state) => state.lapor);
-  const token = localStorage.getItem("token");
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 15;
 
-  useEffect(() => {
-    dispatch(getLapors(token));
-  }, [dispatch, token]);
-
-  async function handleStatus(id, status) {
-    console.log(id, status);
-    const res = await axios.patch(
-      `http://localhost:3000/lapor/${id}/${!status}`,
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-
-    console.log(res);
-  }
-
-  const handleNextPage = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
-  };
-
-  const handlePrevPage = () => {
-    setCurrentPage((prevPage) => prevPage - 1);
-  };
-
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = lapors.slice(indexOfFirstItem, indexOfLastItem);
-
+function Footer() {
   return (
-    <div className="overflow-x-auto bg-[#faffff]">
-      <NavbarAdmin />
-      <div className="mx-auto max-w-4xl">
-        <table className="table mt-8">
-          {/* head */}
-          <thead>
-            <tr>
-              <th className="w-20">No</th>
-              <th className="w-60">Name</th>
-              <th className="w-80">Tanggal</th>
-              <th className="w-80">Keterangan</th>
-              <th className="w-20">Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentItems.map((lapor, index) => (
-              <tr
-                key={index}
-                className={index === 0 ? "" : "border-t-2 border-gray-200"} 
-              >
-                <td>{index + 1 + indexOfFirstItem}</td>
-                <td>
-                  <div className="flex items-center gap-3">
-                    <div>
-                      <div className="font-bold">{lapor?.User?.nama}</div>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  {lapor.tanggal ? lapor.tanggal.substring(0, 10) : "-"}
-                  <br />
-                </td>
-                <td>{lapor.keterangan}</td>
-                <td className="text-center">
-                  <button
-                    className={`px-2 py-1 text-white rounded ${
-                      lapor?.Status?.verified ? "bg-green-500" : "bg-red-500"
-                    } btn btn-ghost btn-xs`}
-                    onClick={() => handleStatus(lapor.id, lapor.Status.verified)}
-                  >
-                    {lapor?.Status?.verified ? "verified" : "unverified"}
-                  </button>
-                </td>
-                <th>
-                  <button className="btn btn-ghost btn-xs">details</button>
-                </th>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {lapors.length > itemsPerPage && (
-        <div className="pagination flex items-center justify-center mt-4 mb-8">
-          <button
-            className="px-3 py-1 bg-gray-200 text-gray-700 rounded-md mr-2"
-            onClick={handlePrevPage}
-            disabled={currentPage === 1}
-          >
-            {currentPage === 1 ? "Prev" : currentPage - 1}
-          </button>
-          <span className="px-3 py-1 bg-blue-400 text-gray-700 rounded-md mr-2">{currentPage}</span>
-          <button
-            className="px-3 py-1 bg-gray-200 text-gray-700 rounded-md mr-2"
-            onClick={handleNextPage}
-            disabled={indexOfLastItem >= lapors.length}
-          >
-            {currentPage + 1}
-          </button>
+    <footer className="footer p-10 bg-[#18978F] text-white font-poppins">
+      <aside className="grid justify-items-center">
+        <img src={logoFooter} width={210} alt="Bubu.id" />
+
+        <nav className="relative left-6">
+          <header className="footer-title text-center font-poppins">
+            Social Media
+          </header>
+          <div className="grid grid-flow-col gap-8">
+            <Link to="">
+              <img src={logoFb}width={24} alt="" />
+            </Link>
+            <Link to="">
+              <img src={logoX} width={24} alt="" />
+            </Link>
+            <Link to="https://www.instagram.com/sman8.batam/" target="_blank">
+              <img src={logoIg} width={24} alt="" />
+            </Link>
+          </div>
+        </nav>
+      </aside>
+      <nav>
+        <header className="footer-title">Daftar</header>
+        <Link to="/" className="link link-hover">
+          Home
+        </Link>
+        <Link to="/aboutbullying" className="link link-hover">
+          About Bullying
+        </Link>
+        <Link to="/lapor" className="link link-hover">
+          Lapor
+        </Link>
+        <Link to="/artikel" className="link link-hover">
+          Artikel
+        </Link>
+      </nav>
+      <nav>
+        <header className="footer-title">Contact</header>
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+          <img src={logophone} width={24} alt="" />
+          <a className="link link-hover ml-3">+62 778 4886205</a>
         </div>
-      )}
-    </div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <img src={logoemail} width={24} alt="" />
+          <a className="link link-hover ml-3">info@sman8btm.sch.id</a>
+        </div>
+        <Link to="/" className="link link-hover"></Link>
+      </nav>
+
+      <nav>
+        <header className="footer-title ">Partner</header>
+        <a href="https://www.sman8btm.sch.id/" target="_blank">
+          <img src={logoSekolah} width={90} alt="Sman8" />
+        </a>
+      </nav>
+    </footer>
   );
 }
 
-export default DataLaporan;
+export default Footer;
