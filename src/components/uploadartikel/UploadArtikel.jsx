34a1ab9e9791a62d4a0722e07b72f6a33c1d6174
kinxyzz <kinxyzz@gmail.com>
2024-05-16 02:59:19 +0700

    feat:adminrole

--- src/components/uploadartikel/UploadArtikel.jsx
+++ src/components/uploadartikel/UploadArtikel.jsx
@@ -24,7 +24,7 @@ export default function UploadArtikel() {
       setLoading(false);
       return;
     }
-
+    //new
     const data = {
       judul,
       isi,
---
+++ src/utils/parseImgName.js
@@ -0,0 +1,12 @@
+export function removeFileExtensionFromUrl(url) {
+  const parts = url.split("/");
+
+  const fileNameWithExt = parts[parts.length - 1];
+
+  const lastDotIndex = fileNameWithExt.lastIndexOf(".");
+  if (lastDotIndex === -1) {
+    return fileNameWithExt;
+  } else {
+    return fileNameWithExt.substring(0, lastDotIndex);
+  }
+}
